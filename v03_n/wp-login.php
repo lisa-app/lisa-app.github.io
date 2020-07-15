0b8651d110b5df6520deb2250553199cd1e0e896
lisa-app <31746502+lisa-app@users.noreply.github.com>
2020-07-15 11:22:52 +0200

    Add files via upload

---
+++ v03_n/wp-login.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="es">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="es">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Acceder &lsaquo; Lisa &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<script src='https://lisa-seguros.com/wp-content/plugins/weglot/dist/front-js.js?ver=3.1.6'></script>
+<link rel='stylesheet' id='dashicons-css'  href='https://lisa-seguros.com/wp-includes/css/dashicons.min.css?ver=5.3.4' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://lisa-seguros.com/wp-includes/css/buttons.min.css?ver=5.3.4' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://lisa-seguros.com/wp-admin/css/forms.min.css?ver=5.3.4' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://lisa-seguros.com/wp-admin/css/l10n.min.css?ver=5.3.4' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://lisa-seguros.com/wp-admin/css/login.min.css?ver=5.3.4' media='all' />
+<link rel='stylesheet' id='weglot-css-css'  href='https://lisa-seguros.com/wp-content/plugins/weglot/dist/css/front-css.css?ver=3.1.6' media='' />
+	<meta name='robots' content='noindex,noarchive' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+	<link rel="icon" href="https://lisa-seguros.com/wp-content/uploads/2020/03/cropped-lisa-icon-32x32.png" sizes="32x32" />
+<link rel="icon" href="https://lisa-seguros.com/wp-content/uploads/2020/03/cropped-lisa-icon-192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="https://lisa-seguros.com/wp-content/uploads/2020/03/cropped-lisa-icon-180x180.png" />
+<meta name="msapplication-TileImage" content="https://lisa-seguros.com/wp-content/uploads/2020/03/cropped-lisa-icon-270x270.png" />
+	</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-es-es">
+	<script type="text/javascript">
+		document.body.className = document.body.className.replace('no-js','js');
+	</script>
+		<div id="login">
+		<h1><a href="https://es.wordpress.org/">Funciona con WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="https://lisa-seguros.com/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Nombre de usuario o correo electrónico</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" />
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Contraseña</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" />
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Mostrar la contraseña">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> <label for="rememberme">Recuérdame</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Acceder" />
+									<input type="hidden" name="redirect_to" value="https://lisa-seguros.com/wp-admin/" />
+									<input type="hidden" name="testcookie" value="1" />
+			</p>
+		</form>
+
+					<p id="nav">
+									<a href="https://lisa-seguros.com/wp-login.php?action=lostpassword">¿Has olvidado tu contraseña?</a>
+								</p>
+					<script type="text/javascript">
+			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
+				<p id="backtoblog"><a href="https://lisa-seguros.com/">
+		&larr; Volver a Lisa		</a></p>
+			</div>
+	<script src='https://lisa-seguros.com/wp-content/plugins/jquery-updater/js/jquery-3.4.1.min.js?ver=3.4.1'></script>
+<script>
+var _zxcvbnSettings = {"src":"https:\/\/lisa-seguros.com\/wp-includes\/js\/zxcvbn.min.js"};
+</script>
+<script src='https://lisa-seguros.com/wp-includes/js/zxcvbn-async.min.js?ver=1.0'></script>
+<script>
+var pwsL10n = {"unknown":"Fortaleza de la contrase\u00f1a desconocida","short":"Muy d\u00e9bil","bad":"D\u00e9bil","good":"Medio","strong":"Fuerte","mismatch":"No coinciden"};
+</script>
+<script src='https://lisa-seguros.com/wp-admin/js/password-strength-meter.min.js?ver=5.3.4'></script>
+<script src='https://lisa-seguros.com/wp-includes/js/underscore.min.js?ver=1.8.3'></script>
+<script>
+var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
+</script>
+<script src='https://lisa-seguros.com/wp-includes/js/wp-util.min.js?ver=5.3.4'></script>
+<script>
+var userProfileL10n = {"warn":"Tu nueva contrase\u00f1a no ha sido guardada.","warnWeak":"Confirma el uso de una contrase\u00f1a d\u00e9bil.","show":"Mostrar","hide":"Ocultar","cancel":"Cancelar","ariaShow":"Mostrar la contrase\u00f1a","ariaHide":"Ocultar la contrase\u00f1a"};
+</script>
+<script src='https://lisa-seguros.com/wp-admin/js/user-profile.min.js?ver=5.3.4'></script>
+	<script>
+	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
+	</script>
+		<div class="clear"></div>
+	</body>
+	</html>
+	
